8b184d7387a5f14136f65dccdfde37302ba2bfa5
markpena02 <79029380+markpena02@users.noreply.github.com>
2024-05-30 06:35:23 +0800

    Add files via upload

---
+++ evaluator/contents/js/print.js
@@ -0,0 +1,24 @@
+// function printForm() {
+//     const printButton = document.querySelector('.no-print');
+//     printButton.style.display = 'none';
+//     window.print();
+//     printButton.style.display = 'block';
+// }
+// function submitForm() {
+//     console.log("submitted form")
+//     // Call the printForm function to print the form
+//     // printForm();
+
+//     // // Assuming your form data is serialized using jQuery
+//     // const formData = $('form').serialize();
+
+//     // $.ajax({
+//     //     type: 'POST',
+//     //     url: '../../controller/evaluator/save_document.php',
+//     //     data: formData,
+//     //     success: function(response) {
+//     //         console.log(response);
+//     //         // $('#response').html(response);
+//     //     }
+//     // });
+// }
