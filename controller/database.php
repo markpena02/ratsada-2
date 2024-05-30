bcca9b16646503bc05226a89d803e4139e5751c8
George Vincent Peña <gvbpena@gmail.com>
2024-05-30 10:51:34 +0800

    evaluator makita na ang makita ang ang gi gifillupan pero dli makita sa proposer kanang db2 gamita

--- controller/database.php
+++ controller/database.php
@@ -2,7 +2,7 @@
     $servername="localhost";
     $username="root";
     $password="";
-    $databasename="gadsystem_db";
+    $databasename="gadsystem_db2";
     $connection = mysqli_connect($servername, $username, $password, $databasename);
     if(!$connection)
     die("Can't connect to database ". mysqli_connect_error());
