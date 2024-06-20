c526371ce1b81c2924365ab8bcab5b6814c09f36
markpena02 <genzo5401@gmail.com>
2024-06-20 11:46:03 +0800

    nabago

--- controller/database.php
+++ controller/database.php
@@ -2,7 +2,7 @@
     $servername="localhost";
     $username="root";
     $password="";
-    $databasename="gadsystem_db2";
+    $databasename="gadsystem_db3";
     $connection = mysqli_connect($servername, $username, $password, $databasename);
     if(!$connection)
     die("Can't connect to database ". mysqli_connect_error());
